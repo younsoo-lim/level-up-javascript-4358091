ab8fbd5411ff13efbfc8efc0b3ee88975767058b
younsoo-icbc <147751039+younsoo-lim@users.noreply.github.com>
2024-04-10 20:20:39 +0000

    original Book function

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,2 +1,32 @@
-// Write your code here
-console.log("Hello world")
+function Book(title, author, ISBN, numCopies) {
+  this.title = title;
+  this.author = author;
+  this.ISBN = ISBN;
+  this.numCopies = numCopies;
+}
+
+Book.prototype.getAvailability = function() {
+  if (this.numCopies === 0) {
+    return "Out of stock";
+  } else if (this.numCopies < 10) {
+    return "Low stock";
+  }
+  return "In stock";
+}
+
+Book.prototype.sell = function(numCopiesSold = 1) {
+  this.numCopies -= numCopiesSold;
+}
+
+Book.prototype.restock = function(numCopiesStocked = 5) {
+  this.numCopies += numCopiesStocked;
+}
+
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(HungerGames.getAvailability());
+
+HungerGames.restock(12);
+console.log(HungerGames.getAvailability());
+
+HungerGames.sell(17);
+console.log(HungerGames.getAvailability());
