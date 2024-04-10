f74d12eefe8e95e58da2f2aebe700a83b28ccf9b
younsoo-icbc <147751039+younsoo-lim@users.noreply.github.com>
2024-04-10 21:46:21 +0000

    finished 01_05

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,13 @@
 // Write your code here
+
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "emmabostian",
+  [password]: "1234566",
+  age: 27,
+};
+
+console.log(user.username)
+console.log(user.password)
