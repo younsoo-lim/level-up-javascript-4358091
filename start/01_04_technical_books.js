01f48ba495ac0988316348e81fcd8195ab9e4fb0
younsoo-icbc <147751039+younsoo-lim@users.noreply.github.com>
2024-04-10 21:35:16 +0000

    finished 01_04

--- start/01_04_technical_books.js
+++ start/01_04_technical_books.js
@@ -31,3 +31,17 @@ class Book {
 }
 
 // Write your code here
+
+class TechnicalBook extends Book {
+  constructor(title, author, ISBN, numCopies, edition) {
+    super(title, author, ISBN, numCopies)
+    this.edition = edition
+  }
+  getEdition() {
+    return `The current version of this book is ${this.edition}`;
+  }
+}
+
+const CrackingTheCodingInterview = new TechnicalBook("Cracking The Coding Interview", "Gayle Laackmann McDowell", 1209123, 7, "2.3");
+console.log(CrackingTheCodingInterview.availability);
+console.log(CrackingTheCodingInterview.getEdition())
